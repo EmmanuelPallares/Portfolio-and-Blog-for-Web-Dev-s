3f5bce68a982ba3f29203a2d2034c952f25c2efa
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-05-13 09:04:13 -0500

    navbar from FCC simple and stetic

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { GraphQLClient, gql } from "graphql-request";
+import { NavBar } from "../components/NavBar";
 import Footer from "../components/Footer";
 const graphcms = new GraphQLClient(
   "https://api-us-west-2.graphcms.com/v2/cl339o8uy4aru01z84ry54y9n/master"
@@ -28,10 +29,10 @@ export async function getStaticProps() {
   };
 }
 
-export default function Index({ portfolios }) {
-  console.log(portfolios)
+export default function Index() {
   return (
     <div class="bg-indigo-700">
+      <NavBar />
       <h1>Hola mundo</h1>
       <Footer />
     </div>
