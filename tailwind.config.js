547c4787905b0d8d0c1eb65cceecd46451ef0dfd
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-05-14 12:17:29 -0500

    postcss tailwind installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  purge: false,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      One: "#E93D44",
-      Two: "#111823",
-      Three: "#222529",
-      Four: "#203449",
+    extends: {
+      colors: {
+        One: "#E93D44",
+        Two: "#111823",
+        Three: "#222529",
+        Four: "#203449",
+      },
     },
   },
   plugins: [],
