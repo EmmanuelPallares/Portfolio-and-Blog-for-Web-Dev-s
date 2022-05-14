111594b6a1ced77c2ec7a0d0d475d05cd249a083
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-05-14 16:58:12 -0500

    fixed the problem with flowbite installation

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,22 @@ import "../styles/globals.css";
 
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
-
-import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    window.welcomeMessage = "Welcome to vitoz.dev!";
-  }, []);
-
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/flowbite@1.4.5/dist/flowbite.min.css"
+        />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
       <Footer />
+
+      <script src="https://unpkg.com/flowbite@1.4.5/dist/flowbite.js"></script>
     </>
   );
 }
