5e632aef6306f50fd45889deb1f32b1eb439df77
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-05-21 14:18:33 -0500

    first map of projects succesfully point of reference

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,4 @@
-import { getPortfolioProps } from "../lib/data";
-
-export const getStaticProps = async () => {
-  const portfolios = await getPortfolioProps();
-
-  return {
-    props: {
-      portfolios,
-    },
-  };
-};
-
-export default function Index(portfolios) {
+export default function Index() {
   return (
     <>
       <div className="max-w-3xl mx-auto px-4 mb-12 text-6xl text-red-500">
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,27 @@
 import ProjectCard from "../components/ProjectCard";
+import { getPortfolioProps } from "../../lib/data";
+export const getStaticProps = async () => {
+  const portfolios = await getPortfolioProps();
 
-export default function MainProjects() {
+  return {
+    props: portfolios,
+  };
+};
+
+export default function MainProjects(portfolios) {
+  const ARRAYPORTFOLIO = portfolios.props.portfolios;
+  console.log(ARRAYPORTFOLIO);
   return (
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-0">
-      hola soy el index de projects
+      {/* MAP OF ARRAYS  */}
+      <div className="text-black text-bold mx-0 p-0 m-6">
+        {ARRAYPORTFOLIO.map(({ title, description }) => (
+          <>
+            <div key={title}>{title} </div>
+            <div className="text-red-600">{description}</div>
+          </>
+        ))}
+      </div>
       <ProjectCard />
     </div>
   );
