2c5f6c8ef78fd34bb5a246f9c5f67151482d8412
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-06-07 08:38:44 -0500

    add a color template in brand page

---
+++ pages/brand/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Brand = () => {
+  return (
+    <>
+      <div className="container m-6 p-3 text-white">
+        <div className="bg-[#E93D44] ">Red Principal</div>
+        <div className="bg-[#751f22]"> Darknes Red</div>
+        <div className="bg-[#AF2E33]"> Medium Red</div>
+      </div>
+      <div className="container m-6 p-3 text-white">
+        <div className="bg-[#111823]">Blue Main</div>
+        <div className="bg-[#203449]">Blue Medium</div>
+        <div className="bg-[#4d5d6D]">Blue Light</div>
+      </div>
+      <div className="container m-6 p-3 text-white">
+        <div className="bg-[#222529]">Darkness Gray</div>
+        <div className="bg-[#4e5154]"> Medium Gray</div>
+        <div className="bg-[#D3D3D4] text-[#111823]"> Light Gray</div>
+      </div>
+    </>
+  );
+};
+
+export default Brand;
