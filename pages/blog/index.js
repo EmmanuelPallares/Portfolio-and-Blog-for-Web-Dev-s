50631db1daaa31b4afcd5b485aa57bb82ecb356d
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-06-10 13:57:27 -0500

    process added to main page for layout x

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -21,7 +21,7 @@ export default function Index(blogs) {
           </h1>
           <ul className="flex text-gray-500 text-sm lg:text-sm pb-12 wow animate__animated animate__fadeIn animated">
             <li className="inline-flex items-center">
-              <a href="#" className="hover:text-blue-500 text-gray-800">
+              <a href="/" className="hover:text-blue-500 text-gray-800">
                 Home
               </a>
               <svg
@@ -37,23 +37,9 @@ export default function Index(blogs) {
               </svg>
             </li>
             <li className="inline-flex items-center">
-              <a href="#" className="hover:text-blue-500 text-gray-800">
+              <a href="/blog" className="hover:text-blue-500 text-gray-800">
                 Blog
               </a>
-              <svg
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                className="h-5 w-auto text-gray-300"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </li>
-            <li className="inline-flex items-center text-gray-400">
-              <span>Web Development</span>
             </li>
           </ul>
           {ARRAYBLOGS.map(
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -20,7 +20,7 @@ export default function Index(portfolios) {
           </h1>
           <ul className="flex text-gray-500 text-sm lg:text-sm pb-12 wow animate__animated animate__fadeIn animated">
             <li className="inline-flex items-center">
-              <a href="#" className="hover:text-blue-500 text-gray-800">
+              <a href="/" className="hover:text-blue-500 text-gray-800">
                 Home
               </a>
               <svg
@@ -36,20 +36,9 @@ export default function Index(portfolios) {
               </svg>
             </li>
             <li className="inline-flex items-center">
-              <a href="#" className="hover:text-blue-500 text-gray-800">
+              <a href="/projects" className="hover:text-blue-500 text-gray-800">
                 Projects
               </a>
-              {/* <svg
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                className="h-5 w-auto text-gray-300"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg> */}
             </li>
             {/* <li className="inline-flex items-center text-gray-400">
               <span>Web Development</span>
