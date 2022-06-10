86a3f83bdb5a5a347f36411c20d28e9bb681a56e
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-06-10 13:01:10 -0500

    projects main view ready

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,4 +1,5 @@
 import { getPortfolioProps } from "../../lib/data";
+import Link from "next/link";
 export const getStaticProps = async () => {
   const portfolios = await getPortfolioProps();
 
@@ -12,174 +13,112 @@ export default function Index(portfolios) {
   console.log(ARRAYPORTFOLIO);
   return (
     <>
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-0">
-        {ARRAYPORTFOLIO.map(
-          ({
-            title,
-            description,
-            slug,
-            imagePortfolioOne,
-            imagePortfolioTwo,
-            imagePortfolioThree,
-          }) => (
-            <div>
-              <div className="mt-6 mb-6  bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <div
-                  id="default-carousel"
-                  className="relative"
-                  data-carousel="static"
-                >
-                  <div className="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-                    <div
-                      className="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20"
-                      data-carousel-item="1"
-                    >
-                      <span className="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">
-                        First Slide
-                      </span>
-                      <img
-                        src={imagePortfolioOne.url}
-                        className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-                        alt="..."
-                      />
-                    </div>
-
-                    <div
-                      className="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-full z-10"
-                      data-carousel-item=""
-                    >
-                      <img
-                        src={
-                          imagePortfolioTwo
-                            ? imagePortfolioTwo.url
-                            : imagePortfolioOne.url
-                        }
-                        className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-                        alt="..."
-                      />
-                    </div>
-
-                    <div
-                      className="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10"
-                      data-carousel-item=""
-                    >
-                      <img
-                        src={
-                          imagePortfolioThree
-                            ? imagePortfolioThree.url
-                            : imagePortfolioOne.url
-                        }
-                        className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
-                        alt="..."
-                      />
-                    </div>
+      <section className="py-20">
+        <div className="container">
+          <h1 className="text-2xl lg:text-5xl font-bold mb-5 wow animate__animated animate__fadeIn animated">
+            Projects and Recent Works
+          </h1>
+          <ul className="flex text-gray-500 text-sm lg:text-sm pb-12 wow animate__animated animate__fadeIn animated">
+            <li className="inline-flex items-center">
+              <a href="#" className="hover:text-blue-500 text-gray-800">
+                Home
+              </a>
+              <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                className="h-5 w-auto text-gray-300"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </li>
+            <li className="inline-flex items-center">
+              <a href="#" className="hover:text-blue-500 text-gray-800">
+                Projects
+              </a>
+              {/* <svg
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                className="h-5 w-auto text-gray-300"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg> */}
+            </li>
+            {/* <li className="inline-flex items-center text-gray-400">
+              <span>Web Development</span>
+            </li> */}
+          </ul>
+          {ARRAYPORTFOLIO.map(
+            ({ title, imagePortfolioOne, slug, description }) => (
+              <>
+                <div className="flex flex-wrap -mx-4 mb-12 wow animate__animated animate__fadeIn animated hover-up-5">
+                  <div className="w-full lg:w-1/2 px-4 lg:pr-20 lg:pt-4 order-1 lg:order-0">
+                    <Link href={`/blog/${slug}`}>
+                      <a>
+                        <span className="inline-block py-1 px-3 uppercase text-xs font-semibold bg-blue-50 rounded-full text-blue-600">
+                          Development
+                        </span>
+                      </a>
+                    </Link>
+                    <h3 className="my-4 text-xl md:text-2xl font-bold font-heading">
+                      <Link href={`/blog/${slug}`}>
+                        <a className="hover:text-blue-500">{title}</a>
+                      </Link>
+                    </h3>
+                    <p className="mb-4 text-sm md:text-base leading-loose text-blueGray-400">
+                      {description}
+                    </p>
+                    <Link href={`/blog/${slug}`}>
+                      <a className="text-xs font-semibold text-blue-600">
+                        <span>Read More</span>
+                        <svg
+                          className="inline-block w-4 h-4 ml-1"
+                          fill="none"
+                          stroke="currentColor"
+                          viewbox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M17 8l4 4m0 0l-4 4m4-4H3"
+                          ></path>
+                        </svg>
+                      </a>
+                    </Link>
                   </div>
-
-                  <div className="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-                    <button
-                      type="button"
-                      className="w-3 h-3 rounded-full bg-white dark:bg-gray-800"
-                      aria-current="true"
-                      aria-label="Slide 1"
-                      data-carousel-slide-to="0"
-                    ></button>
-                    <button
-                      type="button"
-                      className="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
-                      aria-current="false"
-                      aria-label="Slide 2"
-                      data-carousel-slide-to="1"
-                    ></button>
-                    <button
-                      type="button"
-                      className="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800"
-                      aria-current="false"
-                      aria-label="Slide 3"
-                      data-carousel-slide-to="2"
-                    ></button>
+                  <div className="w-full lg:w-1/2 px-4 mb-8 order-0 lg:order-1">
+                    <Link href={`/blog/${slug}`}>
+                      <a>
+                        <img
+                          className="h-80 w-full object-cover rounded"
+                          src={imagePortfolioOne.url}
+                          alt="Monst"
+                        />
+                      </a>
+                    </Link>
                   </div>
-
-                  <button
-                    type="button"
-                    className="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
-                    data-carousel-prev=""
-                  >
-                    <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                      <svg
-                        className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          cliprulestrokelinejoin="round"
-                          strokewidth="2"
-                          d="M15 19l-7-7 7-7"
-                        ></path>
-                      </svg>
-                      <span className="hidden">Previous</span>
-                    </span>
-                  </button>
-                  <button
-                    type="button"
-                    className="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
-                    data-carousel-next=""
-                  >
-                    <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                      <svg
-                        className="w-5 h-5 text-green-100 sm:w-6 sm:h-6 dark:text-red-800"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          cliprulestrokelinejoin="round"
-                          strokewidth="2"
-                          d="M9 5l7 7-7 7"
-                        ></path>
-                      </svg>
-                      <span className="hidden">Next</span>
-                    </span>
-                  </button>
                 </div>
-
-                <div className="p-5">
-                  <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      {title}
-                    </h5>
-                  </a>
-                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {description}
-                  </p>
-                  <a
-                    href="#"
-                    className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Live Demo
-                    <svg
-                      className="ml-2 -mr-1 w-4 h-4"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </div>
-          )
-        )}
-      </div>
+              </>
+            )
+          )}
+          <div className="text-center">
+            <Link href="/blog">
+              <a className="hover-up-5 inline-block py-4 px-8 text-xs text-white font-semibold leading-none bg-blue-400 hover:bg-blue-500 rounded">
+                Show all posts
+              </a>
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
