96aeec0fbd4a3a0f7ab03c4581c228847ad8f669
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2022-06-11 13:20:41 -0500

    changes to the view of blogposts page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -42,61 +42,46 @@ export default function Index(blogs) {
               </a>
             </li>
           </ul>
-          {ARRAYBLOGS.map(
-            ({ blog, index, title, coverImage, slug, content, excerpt }) => (
-              <>
-                <div className="flex flex-wrap -mx-4 mb-12 wow animate__animated animate__fadeIn animated hover-up-5">
-                  <div className="w-full lg:w-1/2 px-4 lg:pr-20 lg:pt-4 order-1 lg:order-0">
-                    <Link href={`/blog/${slug}`}>
+          <div className="flex -mx-3">
+            {ARRAYBLOGS.map(
+              ({ blog, index, title, coverImage, slug, content, excerpt }) => (
+                <div
+                  className="w-full lg:w-1/2 px-3 mb-12 wow animate__animated animate__fadeIn animated hover-up-5"
+                  data-wow-delay=".1s"
+                >
+                  <Link href={`/blog/${slug}`}>
+                    <a>
+                      <img
+                        className="h-80 w-full object-cover rounded"
+                        src={coverImage.url}
+                        alt="Monst"
+                      />
+                    </a>
+                  </Link>
+                  <p className="mt-6 text-sm text-blue-400">
+                    <Link href="/blog-2">
                       <a>
-                        <span className="inline-block py-1 px-3 uppercase text-xs font-semibold bg-blue-50 rounded-full text-blue-600">
-                          Development
+                        <span className="inline-block py-1 px-3 text-xs font-semibold bg-blue-100 text-blue-600 rounded-xl mr-3">
+                          {slug}
                         </span>
                       </a>
                     </Link>
-                    <h3 className="my-4 text-xl md:text-2xl font-bold font-heading">
-                      <Link href={`/blog/${slug}`}>
-                        <a className="hover:text-blue-500">{title}</a>
-                      </Link>
-                    </h3>
-                    <p className="mb-4 text-sm md:text-base leading-loose text-blueGray-400">
-                      {excerpt}
-                    </p>
-                    <Link href={`/blog/${slug}`}>
-                      <a className="text-xs font-semibold text-blue-600">
-                        <span>Read More</span>
-                        <svg
-                          className="inline-block w-4 h-4 ml-1"
-                          fill="none"
-                          stroke="currentColor"
-                          viewbox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M17 8l4 4m0 0l-4 4m4-4H3"
-                          ></path>
-                        </svg>
-                      </a>
+                    <span className="text-blueGray-400 text-xs">
+                      24 Jan, 2021
+                    </span>
+                  </p>
+                  <h3 className="my-2 text-2xl font-bold font-heading">
+                    <Link href="/blog-single">
+                      <a className="hover:text-blue-500">{title}</a>
                     </Link>
-                  </div>
-                  <div className="w-full lg:w-1/2 px-4 mb-8 order-0 lg:order-1">
-                    <Link href={`/blog/${slug}`}>
-                      <a>
-                        <img
-                          className="h-80 w-full object-cover rounded"
-                          src={coverImage.url}
-                          alt="Monst"
-                        />
-                      </a>
-                    </Link>
-                  </div>
+                  </h3>
+                  <p className="text-blueGray-400 leading-loose">
+                    {title + slug}
+                  </p>
                 </div>
-              </>
-            )
-          )}
+              )
+            )}
+          </div>
           <div className="text-center">
             <Link href="/blog">
               <a className="hover-up-5 inline-block py-4 px-8 text-xs text-white font-semibold leading-none bg-blue-400 hover:bg-blue-500 rounded">
