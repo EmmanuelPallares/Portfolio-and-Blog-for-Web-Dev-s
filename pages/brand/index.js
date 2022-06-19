7e7fb738a65bf8b75802656b4698aba070c46cb6
vitoz <67710356+EmmanuelPallares@users.noreply.github.com>
2022-06-19 22:00:47 +0000

    changes to brand page color pallete new ui

--- pages/brand/index.js
+++ pages/brand/index.js
@@ -3,22 +3,6 @@ import React from "react";
 const Brand = () => {
   return (
     <>
-      <div className="mb-6 p-3 text-white">
-        <div className="bg-[#E93D44] ">Red Principal #E93D44</div>
-        <div className="bg-[#751f22]"> Darknes Red #751f22</div>
-        <div className="bg-[#AF2E33]"> Medium Red #AF2E33</div>
-      </div>
-      <div className=" m-6 p-3 text-white">
-        <div className="bg-[#111823]">Blue Main #111823</div>
-        <div className="bg-[#203449]">Blue Medium #203449</div>
-        <div className="bg-[#4d5d6D]">Blue Light #4d5d6D</div>
-      </div>
-      <div className=" m-6 p-3 text-white">
-        <div className="bg-[#222529]">Darkness Gray #222529</div>
-        <div className="bg-[#010303]">Hero gradient image #010303</div>
-        <div className="bg-[#4e5154]"> Medium Gray #4e5154</div>
-        <div className="bg-[#D3D3D4] text-[#111823]">Light Gray #D3D3D4</div>
-      </div>
       <div className="bg-white shadow overflow-hidden sm:rounded-lg">
         <div className="px-4 py-5 sm:px-6">
           <h3 className="text-lg leading-6 font-medium text-gray-900">Brand</h3>
@@ -26,6 +10,23 @@ const Brand = () => {
         </div>
         <div className="border-t border-gray-200">
           <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">About</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                On Tuesday, November 16, 2021, we launched the refreshed
+                Wizeline brand to the world. Our brand is more than a logo. It
+                includes verbal, behavioral and cultural elements, in addition
+                to the visual ones, based on a strategic intent. Together, these
+                elements tell the story of who we are and what we stand for. The
+                Wizeline brand captures our diverse and inclusive, continuous
+                learning culture and the way we partner with our customers to
+                offer them the best solutions for their challenges. On this
+                landing page, you will find resources for applying different
+                elements of our brand easily and consistently. If you need more
+                information or have any questions, please don’t hesitate to
+                contact us.
+              </dd>
+            </div>
             <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#E93D44] ">
               <dt className="text-sm font-medium text-gray-500 ">
                 <div className="text-white">Red Principal</div>
@@ -42,26 +43,32 @@ const Brand = () => {
               <dt className="text-sm font-medium">Medium Red</dt>
               <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#AF2E33</dd>
             </div>
-            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">
-                Salary expectation
-              </dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                $120,000
-              </dd>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#111823] text-white">
+              <dt className="text-sm font-medium">Blue Main</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#111823</dd>
             </div>
-            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">About</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-                incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-                consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-                proident. Irure nostrud pariatur mollit ad adipisicing
-                reprehenderit deserunt qui eu.
-              </dd>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#203449] text-white">
+              <dt className="text-sm font-medium">Blue Medium</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#203449</dd>
+            </div>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#4d5d6D] text-white">
+              <dt className="text-sm font-medium">Blue Light</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#4d5d6D</dd>
+            </div>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#222529] text-white">
+              <dt className="text-sm font-medium">Darkness Gray</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#222529</dd>
+            </div>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#4e5154] text-white">
+              <dt className="text-sm font-medium">Medium Gray</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#4e5154</dd>
+            </div>
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 bg-[#D3D3D4] text-black">
+              <dt className="text-sm font-medium">Light Gray #D3D3D4</dt>
+              <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">#D3D3D4</dd>
             </div>
             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Attachments</dt>
+              <dt className="text-sm font-medium text-gray-500">Assets</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                 <ul
                   role="list"
@@ -69,9 +76,7 @@ const Brand = () => {
                 >
                   <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                     <div className="w-0 flex-1 flex items-center">
-                      <span className="ml-2 flex-1 w-0 truncate">
-                        resume_back_end_developer.pdf
-                      </span>
+                      <span className="ml-2 flex-1 w-0 truncate">logos</span>
                     </div>
                     <div className="ml-4 flex-shrink-0">
                       <a
@@ -84,9 +89,7 @@ const Brand = () => {
                   </li>
                   <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                     <div className="w-0 flex-1 flex items-center">
-                      <span className="ml-2 flex-1 w-0 truncate">
-                        coverletter_back_end_developer.pdf
-                      </span>
+                      <span className="ml-2 flex-1 w-0 truncate">Images</span>
                     </div>
                     <div className="ml-4 flex-shrink-0">
                       <a
