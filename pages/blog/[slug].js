c2c485cdb1ce72c9f8d1889f418a1ff27581c374
vitoz <67710356+EmmanuelPallares@users.noreply.github.com>
2022-06-27 21:01:11 +0000

    slug blog changes

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -67,19 +67,6 @@ const Home = ({ blogItem, VITOZAUTHOR }) => {
                   {blogItem.title}
                 </h2>
               </div>
-              <div className="flex justify-center mb-8">
-                <img
-                  src={VITOZAUTHOR.userImage.url}
-                  alt="Monst"
-                  className="w-12 h-12 object-cover rounded-full"
-                />
-                <div className="pl-4">
-                  <p className="text-blueGray-500 mb-1">{VITOZAUTHOR.name}</p>
-                  <p className="text-xs text-blueGray-500 font-semibold">
-                    {VITOZAUTHOR.company}
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
@@ -93,7 +80,7 @@ const Home = ({ blogItem, VITOZAUTHOR }) => {
                 content={blogItem.richText.raw.children}
                 renderers={{
                   h1: ({ children }) => (
-                    <h1 className="text-blueGray-800 text-3xl text-center wow">
+                    <h1 className="my-12 text-blueGray-800 text-3xl text-center wow">
                       {children}
                     </h1>
                   ),
@@ -150,7 +137,7 @@ const Home = ({ blogItem, VITOZAUTHOR }) => {
                       <title>Logo Twitter</title>
                       <path d="M496 109.5a201.8 201.8 0 01-56.55 15.3 97.51 97.51 0 0043.33-53.6 197.74 197.74 0 01-62.56 23.5A99.14 99.14 0 00348.31 64c-54.42 0-98.46 43.4-98.46 96.9a93.21 93.21 0 002.54 22.1 280.7 280.7 0 01-203-101.3A95.69 95.69 0 0036 130.4c0 33.6 17.53 63.3 44 80.7A97.5 97.5 0 0135.22 199v1.2c0 47 34 86.1 79 95a100.76 100.76 0 01-25.94 3.4 94.38 94.38 0 01-18.51-1.8c12.51 38.5 48.92 66.5 92.05 67.3A199.59 199.59 0 0139.5 405.6a203 203 0 01-23.5-1.4A278.68 278.68 0 00166.74 448c181.36 0 280.44-147.7 280.44-275.8 0-4.2-.11-8.4-.31-12.5A198.48 198.48 0 00496 109.5z"></path>
                     </svg>
-                    <span className="mx-2">twitter</span>
+                    <span className="mx-2">Follow me</span>
                   </a>
                 </div>
               </div>
