6326f262554793016438cc9e090784b925125fa3
unknown <emmanuelpallares50@gmail.com>
2022-07-11 15:48:39 -0500

    google analytics vinculed

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,25 @@ import "../public/assets/css/animate.min.css";
 import "swiper/css";
 import "animate.css";
 import Layout from "./../components/layout/Layout";
-
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Head>
         {/* <Script
           src="../public/assets/vendor/counterup"
